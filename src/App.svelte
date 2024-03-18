57586ee0360ed13b4599b4c3919c1fa60a8442ab
Chet Hill <chetdhill@gmail.com>
2024-03-18 10:07:06 -0500

    Fix issue of components stacking on each other, begin applying custom CSS styles

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,15 @@
 	import ScrollManager from "./components/ScrollManager.svelte";
 </script>
 
+<style>
+	/* ADD STYLING FOR MAIN HERE */
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 20px;
+	}
+</style>
 <main>
 	<ScrollManager />
 </main>
-
-<style>
-	/* ADD STYLING FOR MAIN HERE */
-</style>
--- src/components/ScrollManager.svelte
+++ src/components/ScrollManager.svelte
@@ -3,31 +3,59 @@
     import CanvasComponent from './CanvasComponent.svelte';
 
     let stepIndex = 0;
+    let scroller;
 
     function changeStep(newStep) {
         stepIndex = newStep;
     }
 </script>
-<!-- <Scroller bind:this={scroller} on:section={(event) => updateCanvas(event.detail)}> -->
-<Scroller>
-    <div class="section">Section 1 Content</div>
-    <div class="section">Section 2 Content</div>
-    <div class="section">Section 3 Content</div>
-    <div class="section">Section 4 Content</div>
+
+<Scroller bind:this={scroller} on:section={(event) => changeStep(event.detail)}>
+<!-- <Scroller> -->
+    <div slot="foreground">
+        <div class="scroll-container">
+            <section>
+                <h1>Section 1 Content</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet porta lorem. In at aliquet lorem, at ultrices metus. Vestibulum fermentum sit amet magna porttitor tempus. Phasellus risus lorem, volutpat vitae ullamcorper vel, aliquet in tellus. Proin vel neque imperdiet, tristique justo id, tempor turpis. </p>
+            </section>
+            <section>
+                <h1>Section 2 Content</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet porta lorem. In at aliquet lorem, at ultrices metus. Vestibulum fermentum sit amet magna porttitor tempus. Phasellus risus lorem, volutpat vitae ullamcorper vel, aliquet in tellus. Proin vel neque imperdiet, tristique justo id, tempor turpis. </p>
+            </section>
+            <section>
+                <h1>Section 3 Content</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet porta lorem. In at aliquet lorem, at ultrices metus. Vestibulum fermentum sit amet magna porttitor tempus. Phasellus risus lorem, volutpat vitae ullamcorper vel, aliquet in tellus. Proin vel neque imperdiet, tristique justo id, tempor turpis. </p>
+            </section>
+            <section>
+                <h1>Section 4 Content</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet porta lorem. In at aliquet lorem, at ultrices metus. Vestibulum fermentum sit amet magna porttitor tempus. Phasellus risus lorem, volutpat vitae ullamcorper vel, aliquet in tellus. Proin vel neque imperdiet, tristique justo id, tempor turpis. </p>
+            </section>
+        </div>
+    </div>
+    <div slot="background">
+        <CanvasComponent {stepIndex}></CanvasComponent>
+    </div>
+    
 </Scroller>
 
-<CanvasComponent {stepIndex} />
+
 
 <style>
-    
-
-    .section {
-        position: relative;
-        padding: 20vh 10vw;
-        margin: 40vh 0;
-        border-radius: 5px;
-        background: rgba(255, 255, 255, 0.9);
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        z-index: 10; /* ensure text always above the map */
+    .scroll-container {
+        width: 80%;
+        margin: 0 auto;
+        overflow: auto;
     }
+
+    section {
+        padding: 20px;
+        margin: 10vh 10vw;
+        min-height: 30vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgba(0, 0, 0, 0.5)
+    }
+
+
 </style>
